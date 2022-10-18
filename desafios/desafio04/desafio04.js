4773d4e9980cda24aeb85df3395a7350f49a8f2a
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-18 16:30:39 -0300

    criando desafio 04

---
+++ desafios/desafio04/desafio04.js
@@ -0,0 +1,4 @@
+alert('Este é o desafio 04.')
+var nome = prompt('Digite o seu nome.')
+var desafio = prompt('Digite o número deste desafio.')
+alert(`Olá ${nome}! Seja bem-vindo(a) ao desafio de número ${desafio}! `)
