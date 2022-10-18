a7142349ed55e9d78cbcc210be9bf6b067a89994
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-18 16:45:57 -0300

    adicionando desafio 05

---
+++ desafios/desafio05/desafio05.js
@@ -0,0 +1,5 @@
+var nome = prompt('Digite seu nome.')
+var idade = prompt('Digite sua idade ( apenas números ).')
+var cidade = prompt('Digite a cidade em que você nasceu.')
+var comida = window.prompt('Digite sua comida favorita.')
+window.alert(`${nome} tem ${idade} anos, nasceu em ${cidade} e gosta de comer ${comida}. `)
