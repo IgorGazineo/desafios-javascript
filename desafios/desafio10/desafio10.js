6445e783e556f0dea0186d0a39ff76f54d2f6ee1
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-20 09:27:40 -0300

    adicionando desafio 10

---
+++ desafios/desafio10/desafio10.js
@@ -0,0 +1,3 @@
+nome1 = prompt('Digite seu nome.')
+nome5 = '<br>' + nome1 + '<br>' + nome1 + '<br>' + nome1 + '<br>' + nome1 + '<br>' + nome1 
+document.write(`Seu nome ${nome1} 5x em letras maiúsculas é: ${nome5.toUpperCase()} `)
