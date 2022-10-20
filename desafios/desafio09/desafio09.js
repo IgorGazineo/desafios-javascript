646a48f5a571e72f30e35e155f3efcc10c9d332b
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-20 08:48:13 -0300

    adicionando o desafio 09

---
+++ desafios/desafio09/desafio09.js
@@ -0,0 +1,4 @@
+nome = prompt('Digite seu primeiro nome.')
+sobrenome = prompt(`Olá, ${nome}! Digite seu último sobrenome. `)
+nomeCompleto = nome + sobrenome
+alert(`O seu nome "${nome} ${sobrenome}" tem ${nomeCompleto.length} letras. `)
