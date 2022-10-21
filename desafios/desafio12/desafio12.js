78b2fa1a1f7c317169b4e97c3f7b6381fc9bdd4d
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-21 16:05:03 -0300

    adicionando desafios da aula 04

---
+++ desafios/desafio12/desafio12.js
@@ -0,0 +1,4 @@
+n1 = Number(prompt('Digite um número inteiro.'))
+alert(`O número posterior ao número ${n1} é o número: ${++n1}. `)
+n2 = Number(prompt('Digite outro número inteiro.'))
+alert(`O número anterior ao número ${n2} é o número: ${--n2}.`)
---
+++ desafios/desafio13/desafio13.js
@@ -0,0 +1,4 @@
+n1 = Number(prompt('Digite um número.'))
+n2 = Number(prompt('Digite outro número.'))
+n3 = n1 % n2
+alert(`A divisão inteira dos números ${n1} e ${n2} tem resto ${n3}. `)
