4465e24787dcf81d109bdce534d63d7e2d2666f3
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-21 16:17:20 -0300

    finazilando os exercícios da aula 04

---
+++ desafios/desafio14/desafio14.js
@@ -0,0 +1,8 @@
+n = Number(prompt('Digite um número'))
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+document.write(`A tabuada de ${n} é:<br> ${n} x 1 = ${n1}<br>${n} x 2 = ${n2}<br>${n} x 3 = ${n3}<br>${n} x 4 = ${n4}<br>${n} x 5 = ${n5}<br>${n} x 6 = ${n6}<br>`)
