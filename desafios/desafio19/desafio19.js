e93ee8cc2db864620241fdc1549608199774a4d3
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-25 12:46:58 -0300

    adicionando o desafio 19

---
+++ desafios/desafio19/desafio19.js
@@ -0,0 +1,7 @@
+alert('Você vai escolher as cores da letra e do fundo do site.')
+confirm('Coloque valores iguais aos do CSS. Ex: red, blue, #202124, rgb(255, 110, 0)')
+let corFundo = prompt('Digite a cor de fundo do site.')
+let corLetra = prompt('Digite a cor da letra do site.')
+document.getElementsByTagName('body')[0].style.background = corFundo
+document.getElementsByClassName('color')[0].style.color = corLetra
+document.getElementsByClassName('color')[1].style.color = corLetra
