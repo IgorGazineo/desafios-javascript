9f7c1db55f90407796fee4976e95fa4dd8d8a53d
IgorGazineo <gaumgazineo@hotmail.com>
2022-10-28 09:22:56 -0300

    adicionando aula 07

--- desafios/desafio19/desafio19.js
+++ desafios/desafio19/desafio19.js
@@ -3,5 +3,6 @@ confirm('Coloque valores iguais aos do CSS. Ex: red, blue, #202124, rgb(255, 110
 let corFundo = prompt('Digite a cor de fundo do site.')
 let corLetra = prompt('Digite a cor da letra do site.')
 document.getElementsByTagName('body')[0].style.background = corFundo
-document.getElementsByClassName('color')[0].style.color = corLetra
 document.getElementsByClassName('color')[1].style.color = corLetra
+document.getElementsByClassName('color')[2].style.color = corLetra
+document.getElementsByClassName('color')[0].style.border = `1px solid ${corLetra}`
